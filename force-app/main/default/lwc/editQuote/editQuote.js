a16bc6c146a8f153d56c2e85e538d3a6ada712ac
Prateek-Mordani <mordaniprateek04@gmail.com>
2023-03-23 12:29:29 +0530

    changes

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -4,13 +4,41 @@
  */
 
 import { LightningElement, api } from "lwc";
-
+import getQuoteData from '@salesforce/apex/QuoteDto.getOrUpdateQuote';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
-  };
+  endDate;
+  startDate;
+
+  connectedCallback(){
+    getQuoteData({recordId:this.recordId, startDate:this.startDate , endDate:this.endDate}).then(result=>{
+        console.log(JSON.parse(JSON.stringify(result)));
+        this.endDate = result.endDate;
+        this.startDate = result.startDate;
+    })
+  }
+  handleChange(event){
+    if(event.target.label == 'Start Date'){
+      this.startDate = event.target.value;
+      console.log('startDate',this.startDate);
+    }
+    else{
+      this.endDate = event.target.value;
+      console.log('endDate',this.endDate);
+    }
+  }
+  handleSave(){
+    getQuoteData({recordId:this.recordId, startDate:this.startDate , endDate:this.endDate}).then(result=>{
+      console.log(result);
+      const event = new ShowToastEvent({
+        title: 'Record updated',
+        message:
+            'Updation successful',
+    });
+    this.dispatchEvent(event);
+    })
+  }
 
   renderedCallback() {}
 }
